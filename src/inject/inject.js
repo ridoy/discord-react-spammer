513d85059bc5931552bc3a0f5f6255a8d1a27207
Ridoy Majumdar <majumdar.ridoy@gmail.com>
2020-09-12 00:37:35 -0700

    Grab messages after discord changed message class names

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,18 +2,24 @@
 const EMOJI_NAME = ":heart:"
 console.log(EMOJI_NAME);
 
-document.arrive('#messages-0', function() {
-    let messages = $('[id^=messages]');
+document.arrive('[class^="message-"]', {onceOnly: true}, function() {
+    let messages = $('[class^="message-"]');
     for (let message of messages) {
         $(message).focus()
     }
     let reactionButtons = $('[aria-label="Add Reaction"]')
+    console.log(reactionButtons)
     for (let i = reactionButtons.length - 1; i >= 0; i--) {
         let reactionButton = reactionButtons[i];
         let time = (reactionButtons.length - 1 - i) * 500;
         setTimeout(function() {
             reactionButton.click();
             console.log('clicking button time ' + time + 'ms');
+            /*
+            let objectsCategoryButton = $('[aria-label="objects"]')
+            objectsCategoryButton.click();
+            */
+
             let emojiSelector = $('[aria-label="' + EMOJI_NAME + '"]')
             emojiSelector.click()
         }, time);
