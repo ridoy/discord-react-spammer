101c04a74ec3ba4179136f0ecdc151724f499914
ridoy <majumdar.ridoy@gmail.com>
2023-05-03 01:19:24 -0400

    new approach

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,13 +1,101 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+    function(request, sender, sendResponse) {
+        chrome.pageAction.show(sender.tab.id);
+        sendResponse();
+    });
+
+
+// Before we can start, we need
+// First message id, channel id
+// Authorization header and everything else
+// Then when ready, hit new messages endpoint, hit reaction endpoint slowly
+
+let authToken = null;
+let channelId = ;
+let mostRecentMessageId = ;
+
+/*
+ * Scan requests until we find one with an auth token.
+ */
+chrome.webRequest.onSendHeaders.addListener(
+    function(request) {
+        if (authToken) return;
+        let auth = request.requestHeaders.filter((h) => h.name === "Authorization");
+        if (auth.length > 0) {
+            authToken = auth[0].value;
+            startPosting();
+        }
+    },
+    { urls: ["https://discord.com/*"] },
+    ['requestHeaders']
+);
+
+function startPosting() {
+    let messages = getMessages() 
+        .then((response) => {
+            const reader = response.body.getReader();
+            return new ReadableStream({
+                start(controller) {
+                    function push() {
+                        reader.read().then(({ done, value }) => {
+                            if (done) {
+                                controller.close();
+                                return;
+                            }
+                            controller.enqueue(value);
+                            push();
+                        });
+                    }
+                    push();
+                },
+            });
+        })
+        .then((stream) =>
+            new Response(stream, { headers: { "Content-Type": "application/json" } }).text()
+        )
+        .then((messages) => {
+            const messageIds = JSON.parse(result).map(entry=>entry.id);
+            addReactions();
+        });
+}
+
+function getMessages() {
+    return fetch(`https://discord.com/api/v9/channels/${channelId}/messages?before=${mostRecentMessageId}&limit=50`, {
+        "headers": {
+            "authorization": authToken,
+        },
+        "body": null,
+        "method": "GET",
+        "mode": "cors",
+        "credentials": "include"
+    });
+}
+
+function addReactions(ids) {
+    if (ids.length <= 0) return;
+    let nextId = ids.pop();
+    addReaction(nextId);
+
+    console.log(ids.length);
+
+    setTimeout(() => { 
+        addReactions(ids) 
+    }, 1000);
+
+}
+
+function addReaction(id) {
+    return fetch(`https://discord.com/api/v9/channels/${channelId}/messages/${id}/reactions/%F0%9F%98%B3/%40me?location=Message&type=0`, {
+        "headers": {
+            "authorization": authToken,
+        },
+        "body": null,
+        "method": "PUT",
+        "mode": "cors",
+        "credentials": "include"
+    })
+        .then((res) => console.log(res))
+        .catch(e => console.log(e, id));
+}
+
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -15,10 +15,50 @@ const EMOJI_NAME = "flushed"
 
 // Other constants
 const BUTTON_WRAPPER_CLASS = "wrapper-2vIMkT";
+const DEBUG_MODE = true;
 
 console.log(`Discord React Spammer has loaded and will attempt to spam ${EMOJI_NAME}.`);
 // Wait for messages to load.
-document.arrive('[class^="message-"]', {onceOnly: true}, function() {
+
+function check()  {
+    console.log(document.querySelectorAll(`.${BUTTON_WRAPPER_CLASS}`));
+    setTimeout(check, 300);
+}
+
+
+
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+      console.log(request, sender, sendResponse)
+  }
+);
+
+let messages = new Set();
+
+/*
+setInterval(function() {
+    let messageElements = document.querySelectorAll("[id^='chat-messages-400411213948780545']");
+    let newMessages = [];
+    messageElements.forEach(msgEl => {
+        let msgId = msgEl.id.replace('chat-messages-400411213948780545-', '');
+        if (!messages.has(msgId)) {
+            newMessages.push(msgId);
+            messages.add(msgId);
+        }
+    });
+    
+    if (newMessages.length > 0) {
+        console.log(newMessages);
+        //sendToQueue(newMessages);
+    }
+
+}, 1000);
+*/
+
+//check();
+/*
+document.arrive(`[class^=${BUTTON_WRAPPER_CLASS}]`, {onceOnly: true}, function() {
     let messages = $('[class^="message-"]');
     for (let message of messages) {
         $(message).focus();
@@ -26,10 +66,10 @@ document.arrive('[class^="message-"]', {onceOnly: true}, function() {
 
     // Open Add Reaction menu for each visible message
     // Apply emoji to each message in reverse chronological order
-    
     let buttonWrappers = document.querySelectorAll(`.${BUTTON_WRAPPER_CLASS}`);
     let reactionButtons = [];
     buttonWrappers.forEach((wrapper) => reactionButtons.push(wrapper.firstChild));
+    console.log(buttonWrappers, reactionButtons);
     for (let i = reactionButtons.length - 1; i >= 0; i--) {
         let reactionButton = reactionButtons[i];
         let time = (reactionButtons.length - 1 - i) * 500;
@@ -37,10 +77,26 @@ document.arrive('[class^="message-"]', {onceOnly: true}, function() {
         // We generally want to avoid being rate-limited by Discord.
         // TODO Implement retries for requests that receive a 429 response
         setTimeout(function() {
+            debug("Reached timeout");
             reactionButton.click();
-            let emojiSelector = $('[data-name="' + EMOJI_NAME + '"]');
-            emojiSelector.click();
+            retryClick();
         }, time);
     }
 });
+*/
+function retryClick() {
+    setTimeout(function() {
+        const emojiSelector = document.querySelector(`[data-name="${EMOJI_NAME}"]`);
+        if (!emojiSelector) {
+            retryClick();
+        } else {
+            emojiSelector.click();
+        }
+        return;
+    }, 10);
+}
 
+
+function debug(msg) {
+    if (DEBUG_MODE) console.log(msg);
+}
